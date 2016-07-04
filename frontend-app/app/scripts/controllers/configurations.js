690005d497d55f4d5965e8f070fa136bd5abc208
Daniel Carlson <dancarl758@gmail.com>
2016-07-04 05:39:59 +0100

    Finishing first version of Configurations page

--- frontend-app/app/scripts/controllers/configurations.js
+++ frontend-app/app/scripts/controllers/configurations.js
@@ -3,9 +3,15 @@
 
 angular.module('mobileMoneyApp')
   .controller('configCtrl', ['$scope', function ($scope) {
+  	// activate collapsibles on UI
   	 $(document).ready(function(){
   	 	$('.collapsible').collapsible({
   	 		accordion: false
+  	 	});
+
+  	 	// activate select form fields on UI
+  	 	$(document).ready(function(){
+  	 		$('select').material_select();
   	 	});
   	 });  
 
