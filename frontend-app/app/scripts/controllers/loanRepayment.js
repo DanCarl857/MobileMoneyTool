a5648f35dc5c69a38b94f777cd02fcdbfdf08981
Daniel Carlson <dancarl758@gmail.com>
2016-07-13 22:00:11 +0100

    Adding UI for loan repayments

--- frontend-app/app/scripts/controllers/loanRepayment.js
+++ frontend-app/app/scripts/controllers/loanRepayment.js
@@ -2,6 +2,25 @@
 /* global $ */
 
 angular.module('mobileMoneyApp')
-	.controller('loanCtrl', [{
+	.controller('loanCtrl', ['$scope', function($scope){
 
+		$scope.submitted = true;
+        var baseUrl = "http://localhost:8090/api/v1/loans";
+
+        // function to submit the form after all form validation
+        $scope.submitLoanForm = function(){
+          // Check to make sure the form is completely valid
+          console.log("testing loans");
+          if($scope.loanForm.$valid){
+            $scope.submitted = false;
+            $scope.loanRepayment(12345678);
+          }
+        };
+        
+        $scope.amount = '';
+        $scope.phoneNumber = '';
+
+        $scope.goBack = function(){
+        	window.history.back();
+        }
 	}]);
